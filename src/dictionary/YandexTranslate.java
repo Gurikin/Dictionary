5ffb79098e904ed57e061804b0eeee424b7600f7
Gurikin <gurikin@yandex.ru>
2016-08-30 23:37:43 +0300

    Update YandexTranslate.java

--- src/dictionary/YandexTranslate.java
+++ src/dictionary/YandexTranslate.java
@@ -31,17 +31,14 @@ import java.net.URLEncoder;
 public class YandexTranslate {
     private static int i = 0;
 	public static String translate(String lang, String input) throws IOException {
-		//old translate api key
-                //String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
-                //my translate api key
+		//my translate api key
                 String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160830T202008Z.dca20e7c532187a4.b563037b8a5b88bbeef003b16684365989a69a16";
 		URL urlObj = new URL(urlStr);
 		HttpsURLConnection connection = (HttpsURLConnection)urlObj.openConnection();
 		connection.setRequestMethod("POST");
 		connection.setDoOutput(true);
 		DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-		//dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);// + "&format=plain");
-                dataOutputStream.writeBytes("&text=" + input + "&lang=" + lang + "&format=plain");
+		dataOutputStream.writeBytes("&text=" + input + "&lang=" + lang + "&format=plain");
 
 		InputStream response = connection.getInputStream();
 		String json = new java.util.Scanner(response).nextLine();
